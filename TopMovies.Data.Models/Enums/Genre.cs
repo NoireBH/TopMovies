c35340855c6eb0514b5bc7c2886add0d13057b74
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-01-25 11:53:02 +0200

    removed genre class and made it into an enum instead

---
+++ TopMovies.Data.Models/Enums/Genre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopMovies.Data.Models.Enums
+{
+	public enum Genre
+	{
+		Action = 0,
+		Animation = 1,
+		Comedy = 2,
+		Crime = 3,
+		Drama = 4,
+		Fantasy = 5,
+		Horror = 6,
+		SciFi = 7,
+		Thriller = 8,
+		History = 9
+	}
+}
