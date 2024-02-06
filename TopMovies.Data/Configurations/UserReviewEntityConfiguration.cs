89f6b1e42713766b5a0411bfd837b7a577a7a135
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-02-06 20:43:26 +0200

    added userReviewSeeding

--- TopMovies.Data/Configurations/UserReviewEntityConfiguration.cs
+++ TopMovies.Data/Configurations/UserReviewEntityConfiguration.cs
@@ -37,11 +37,23 @@ namespace TopMovies.Data.Configurations
 
 			userReview = new UserReview()
 			{
-				Id = new Guid("ECD7D309-AA53-44BB-B876-27A6BF213DC5"),
+				Id = new Guid("176697C8-9480-447A-9E86-07B70B0A6585"),
 				ApplicationUserId = Guid.Parse("735B0AD2-82A1-4394-A521-2B1DC921C737"),
-				Rating = 10,
-				Comment = "One of the best movies I've ever seen!",
-				MovieId = Guid.Parse("2CA61990-FE17-483C-863D-442EE4C0ACAD")
+				Rating = 9,
+				Comment = "A classic! My only complaint is that the pacing was too slow for my taste.If you have ADHD like me you're gonna have a hard time watching this movie, but i still think it's worth watching!",
+				MovieId = Guid.Parse("30DA4459-9475-404D-92FB-9124CFF7CFA6")
+
+			};
+
+			userReviews.Add(userReview);
+
+			userReview = new UserReview()
+			{
+				Id = new Guid("FBD5E0AF-4C33-411B-A0B3-88D7863F9D6E"),
+				ApplicationUserId = Guid.Parse("79228DC5-F961-4645-9FD7-E3BC820875E0"),
+				Rating = 7,
+				Comment = "While good it's extremely overrated",
+				MovieId = Guid.Parse("30DA4459-9475-404D-92FB-9124CFF7CFA6")
 
 			};
 
@@ -52,8 +64,8 @@ namespace TopMovies.Data.Configurations
 				Id = new Guid("ECD7D309-AA53-44BB-B876-27A6BF213DC5"),
 				ApplicationUserId = Guid.Parse("735B0AD2-82A1-4394-A521-2B1DC921C737"),
 				Rating = 10,
-				Comment = "One of the best movies I've ever seen!",
-				MovieId = Guid.Parse("2CA61990-FE17-483C-863D-442EE4C0ACAD")
+				Comment = "Peak fiction, they don't make dark batman movies like this anymore...",
+				MovieId = Guid.Parse("075AF806-FB6F-476D-804F-BAECBD15BDD3")
 
 			};
 
@@ -61,23 +73,11 @@ namespace TopMovies.Data.Configurations
 
 			userReview = new UserReview()
 			{
-				Id = new Guid("ECD7D309-AA53-44BB-B876-27A6BF213DC5"),
-				ApplicationUserId = Guid.Parse("735B0AD2-82A1-4394-A521-2B1DC921C737"),
-				Rating = 10,
-				Comment = "One of the best movies I've ever seen!",
-				MovieId = Guid.Parse("2CA61990-FE17-483C-863D-442EE4C0ACAD")
-
-			};
-
-			userReviews.Add(userReview);
-
-			userReview = new UserReview()
-			{
-				Id = new Guid("ECD7D309-AA53-44BB-B876-27A6BF213DC5"),
-				ApplicationUserId = Guid.Parse("735B0AD2-82A1-4394-A521-2B1DC921C737"),
-				Rating = 10,
-				Comment = "One of the best movies I've ever seen!",
-				MovieId = Guid.Parse("2CA61990-FE17-483C-863D-442EE4C0ACAD")
+				Id = new Guid("96475262-852C-442F-B88C-8642835ECB30"),
+				ApplicationUserId = Guid.Parse("79228DC5-F961-4645-9FD7-E3BC820875E0"),
+				Rating = 4,
+				Comment = "Not that great. Didn't remain consistent with Batman Begins and was extremely predictable. The plot and action labors along and never reaches a satisfying conclusion. The best thing about this film is the score.\r\n",
+				MovieId = Guid.Parse("075AF806-FB6F-476D-804F-BAECBD15BDD3")
 
 			};
 
