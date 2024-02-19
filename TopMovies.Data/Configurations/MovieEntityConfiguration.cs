7b45cd4be221b35d2fbc8f3fbfc90e091de088b0
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-02-19 19:15:40 +0200

    added more actors and movies

--- TopMovies.Data/Configurations/MovieEntityConfiguration.cs
+++ TopMovies.Data/Configurations/MovieEntityConfiguration.cs
@@ -101,45 +101,45 @@ namespace TopMovies.Data.Configurations
                 Id = Guid.Parse("506F4021-595B-4637-BBA0-E2B88377549E"),
                 Name = "It",
                 Description = "In the summer of 1989, a group of bullied kids band together to destroy a shape-shifting monster, which disguises itself as a clown and preys on the children of Derry, their small Maine town.\r\n\r\n",
-                ReleaseDate = new DateTime(1994, 5, 21),
+                ReleaseDate = new DateTime(2017, 9, 5),
                 Rating = 7.3,
-                ImageUrl = "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1055_.jpg"
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BZDVkZmI0YzAtNzdjYi00ZjhhLWE1ODEtMWMzMWMzNDA0NmQ4XkEyXkFqcGdeQXVyNzYzODM3Mzg@._V1_.jpg"
             };
 
             movies.Add(movie);
 
             movie = new Movie()
             {
-                Id = Guid.Parse("1F2E3281-95EA-4E2E-8693-D4983C7907DA"),
-                Name = "Pulp Fiction",
-                Description = "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-                ReleaseDate = new DateTime(1994, 5, 21),
-                Rating = 8.9,
-                ImageUrl = "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1055_.jpg"
+                Id = Guid.Parse("3D777BDC-DED7-42D2-B0A6-FC378EB32173"),
+                Name = "Drive",
+                Description = "A mysterious Hollywood action film stuntman gets in trouble with gangsters when he tries to help his neighbor's husband rob a pawn shop while serving as his getaway driver.\r\n\r\n",
+                ReleaseDate = new DateTime(2011, 5, 20),
+                Rating = 7.8,
+                ImageUrl = "https://m.media-amazon.com/images/M/MV5BZjY5ZjQyMjMtMmEwOC00Nzc2LTllYTItMmU2MzJjNTg1NjY0XkEyXkFqcGdeQXVyNjQ1MTMzMDQ@._V1_FMjpg_UX1000_.jpg"
             };
 
             movies.Add(movie);
 
             movie = new Movie()
             {
-                Id = Guid.Parse("1F2E3281-95EA-4E2E-8693-D4983C7907DA"),
-                Name = "Pulp Fiction",
-                Description = "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-                ReleaseDate = new DateTime(1994, 5, 21),
-                Rating = 8.9,
-                ImageUrl = "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1055_.jpg"
+                Id = Guid.Parse("E054427D-CA00-4BFC-921E-AE86FBF621EF"),
+                Name = "Barbie",
+                Description = "Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.\r\n\r\n",
+                ReleaseDate = new DateTime(2023, 7, 9),
+                Rating = 6.9,
+                ImageUrl = "https://m.media-amazon.com/images/M/MV5BNjU3N2QxNzYtMjk1NC00MTc4LTk1NTQtMmUxNTljM2I0NDA5XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg"
             };
 
             movies.Add(movie);
 
             movie = new Movie()
             {
-                Id = Guid.Parse("1F2E3281-95EA-4E2E-8693-D4983C7907DA"),
-                Name = "Pulp Fiction",
-                Description = "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-                ReleaseDate = new DateTime(1994, 5, 21),
-                Rating = 8.9,
-                ImageUrl = "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1055_.jpg"
+                Id = Guid.Parse("75CC9CFD-225C-4F69-8870-3986E74A88A4"),
+                Name = "Blade Runner 2049",
+                Description = "Young Blade Runner K's discovery of a long-buried secret leads him to track down former Blade Runner Rick Deckard, who's been missing for thirty years.",
+                ReleaseDate = new DateTime(2017, 10, 3),
+                Rating = 8.0,
+                ImageUrl = "https://m.media-amazon.com/images/M/MV5BNzA1Njg4NzYxOV5BMl5BanBnXkFtZTgwODk5NjU3MzI@._V1_FMjpg_UX1000_.jpg"
             };
 
             movies.Add(movie);
