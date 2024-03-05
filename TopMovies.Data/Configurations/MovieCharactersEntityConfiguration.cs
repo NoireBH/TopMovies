0e4369999922c7235760fb3af0eee2cd23615299
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-03-05 18:48:27 +0200

    added movieCharacter seeder

--- TopMovies.Data/Configurations/MovieCharactersEntityConfiguration.cs
+++ TopMovies.Data/Configurations/MovieCharactersEntityConfiguration.cs
@@ -26,7 +26,79 @@ namespace TopMovies.Data.Configurations
 			{
 				Id = 1,
 				Name = "Andy Dufresne",
-				ImageUrl = ""
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BZTZlNGQ5MjAtZmE3OC00MWFlLWE2MWItYWY2ZTIwOTZlNDYxXkEyXkFqcGdeQXVyMzQ3Nzk5MTU@._V1_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 2,
+				Name = "Ellis Boyd 'Red' Redding",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BZGVmZjg5NTEtMjJhNS00OWJjLThjOTYtZTk0NjUwMjA3MjkyXkEyXkFqcGdeQXVyMzQ3Nzk5MTU@._V1_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 3,
+				Name = "Captain Hadley",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BODUzYTVhM2EtODVjOS00ODFkLWFjNjItNGEzOWFmNTgyZDI2XkEyXkFqcGdeQXVyOTc5MDI5NjE@._V1_FMjpg_UX600_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 4,
+				Name = "Carlo Rizzi",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BYjhiMGExMmYtM2RlOC00OTFmLWFmODQtMjkxMjZkNjk0ZTIzXkEyXkFqcGdeQXVyMTIwODk1NTQ@._V1_FMjpg_UX600_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 5,
+				Name = "Michael Corleone",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMDhkYzhiYjEtMTQwYy00MTdmLTlkOTAtYTk1YmMyYmE5NDVmXkEyXkFqcGdeQXVyMDc2NTEzMw@@._V1_FMjpg_UY337_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 6,
+				Name = "Kay Adams",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BYmMyZTNiZjItMjZkOC00NDRkLTg5YTEtZDFjODBlODliYjYxXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 7,
+				Name = "Bruce Wayne, Batman",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BYmMyZTNiZjItMjZkOC00NDRkLTg5YTEtZDFjODBlODliYjYxXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 8,
+				Name = "Kay Adams",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BYmMyZTNiZjItMjZkOC00NDRkLTg5YTEtZDFjODBlODliYjYxXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 9,
+				Name = "Kay Adams",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BYmMyZTNiZjItMjZkOC00NDRkLTg5YTEtZDFjODBlODliYjYxXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg",
 			};
 
 			movieCharacters.Add(movieCharacter);
