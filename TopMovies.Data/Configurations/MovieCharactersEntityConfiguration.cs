c53b407f0ec6e361020c413c70c5a1822d4fbf0c
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-03-06 17:54:04 +0200

    added more data to seed method

--- TopMovies.Data/Configurations/MovieCharactersEntityConfiguration.cs
+++ TopMovies.Data/Configurations/MovieCharactersEntityConfiguration.cs
@@ -88,8 +88,8 @@ namespace TopMovies.Data.Configurations
 			movieCharacter = new MovieCharacter()
 			{
 				Id = 8,
-				Name = "Kay Adams",
-				ImageUrl = "https://m.media-amazon.com/images/M/MV5BYmMyZTNiZjItMjZkOC00NDRkLTg5YTEtZDFjODBlODliYjYxXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg",
+				Name = "Joker",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMjA5ODU3NTI0Ml5BMl5BanBnXkFtZTcwODczMTk2Mw@@._V1_FMjpg_UX2048_.jpg",
 			};
 
 			movieCharacters.Add(movieCharacter);
@@ -97,8 +97,197 @@ namespace TopMovies.Data.Configurations
 			movieCharacter = new MovieCharacter()
 			{
 				Id = 9,
-				Name = "Kay Adams",
-				ImageUrl = "https://m.media-amazon.com/images/M/MV5BYmMyZTNiZjItMjZkOC00NDRkLTg5YTEtZDFjODBlODliYjYxXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg",
+				Name = "Scarecrow",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BY2FkYmExNTMtYTkxNS00NzA0LTliYTktOTNjNjEwZmRlMTU2XkEyXkFqcGdeQXVyNDIyNjA2MTk@._V1_.jpgg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 10,
+				Name = "Adam",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMTUxMDgyMTg3N15BMl5BanBnXkFtZTcwMDY2OTYwNA@@._V1_FMjpg_UX2048_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 11,
+				Name = "Dr. Lawrence Gordon",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BZDdmMDA3NzItYmNlYi00NjU3LWJlZGYtMDYxNzg0OWUzNDJjXkEyXkFqcGdeQXVyNjQ4ODE4MzQ@._V1_FMjpg_UX1280_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 12,
+				Name = "Jigsaw",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMTM0MzIwMTI5OV5BMl5BanBnXkFtZTcwOTg1NjAxNQ@@._V1_FMjpg_UX2048_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 13,
+				Name = "Vincent Vega",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BYTMzOWFiMTEtZGFjOS00OWEyLTlhYmEtOGRlZmNkZjhhNmJhXkEyXkFqcGdeQXVyNDIyNjA2MTk@._V1_FMjpg_UX2048_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 14,
+				Name = "Mia Wallace",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BNzg1MTgwMTUzM15BMl5BanBnXkFtZTcwNjA0MzU5Ng@@._V1_FMjpg_UX2048_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 15,
+				Name = "Jules Winnfield",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMjIwNTg0Mjc4Nl5BMl5BanBnXkFtZTcwODAzNDY3Mw@@._V1_FMjpg_UX660_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 16,
+				Name = "Mike",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BZjkwZTllMzgtMDAyOC00MDgzLWE4NmUtOTZmNzQ5NTY1NGIzXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_FMjpg_UX1920_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 17,
+				Name = "Vanessa",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BNzdkNjBmYzItMDZjMC00ZTFkLThmYWYtNDY5OWNiNTZlZjViXkEyXkFqcGdeQXVyMTY2MDI5OTg3._V1_FMjpg_UX1170_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 18,
+				Name = "William Afton",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMzc1N2M0MWQtZTQ3NS00MmVmLWE1NDctOTg5MWQ0ZmEyMjk2XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_FMjpg_UX1920_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 19,
+				Name = "Pennywise",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMTg1NTU5NTgwOV5BMl5BanBnXkFtZTgwMTQ1NzMzMzI@._V1_FMjpg_UX1280_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 20,
+				Name = "Richie Tozier",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMTk2Nzg4NzUyMF5BMl5BanBnXkFtZTgwNDM1NzMzMzI@._V1_FMjpg_UX2048_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 21,
+				Name = "Bill Denbrough",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMjE2MTg2OTM0Nl5BMl5BanBnXkFtZTgwNTM1NzMzMzI@._V1_FMjpg_UX2048_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 22,
+				Name = "Driver",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BNTM1NDk2OTE3M15BMl5BanBnXkFtZTcwMTM1NTgxNQ@@._V1_FMjpg_UX2048_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 23,
+				Name = "Shannon",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BNDgwMDMxMzQ5MF5BMl5BanBnXkFtZTcwMTEwMDk3NQ@@._V1_FMjpg_UX2048_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 24,
+				Name = "Irene",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMTc2NTI1MzU5NF5BMl5BanBnXkFtZTcwMDAwMDk3NQ@@._V1_FMjpg_UX2048_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 25,
+				Name = "Barbie",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BZTM1NzVmZTItYmU0Zi00ODVmLWE4MWQtZTFlMzRiZmMxN2I4XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UY5791_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 26,
+				Name = "Ken",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMmY1NjBlNWEtZjNlZS00YmY3LWE5MTAtOWE3ZjAzYmQ3OTJjXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_FMjpg_UY4976_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 27,
+				Name = "Weird Barbie",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMDE1ZjJmMTMtYmJlZi00N2VjLWFkMmMtZTNjMDdjMGQ1NzU5XkEyXkFqcGdeQXVyMjY1OTQ0MDA@._V1_FMjpg_UX2160_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 28,
+				Name = "'K'",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMTA0MzA5MjU4ODFeQTJeQWpwZ15BbWU4MDYzOTI2NjMy._V1_FMjpg_UX1280_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 29,
+				Name = "Rick Deckard",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BMjQ1MzQ2NTM3Ml5BMl5BanBnXkFtZTgwMzI5MjY2MzI@._V1_FMjpg_UX2048_.jpg",
+			};
+
+			movieCharacters.Add(movieCharacter);
+
+			movieCharacter = new MovieCharacter()
+			{
+				Id = 30,
+				Name = "Sapper Morton",
+				ImageUrl = "https://m.media-amazon.com/images/M/MV5BZjE4MzcyMzMtZDM0ZS00ZWQ4LTg2N2MtMGEwZDE1OTBkNTYxXkEyXkFqcGdeQXVyNzg2ODI2OTU@._V1_FMjpg_UX2160_.jpg",
 			};
 
 			movieCharacters.Add(movieCharacter);
