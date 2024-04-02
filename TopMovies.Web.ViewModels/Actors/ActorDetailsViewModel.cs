7feafeb58fa20a0412fbbcf0f48eaa9a232b69dc
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-04-02 18:42:05 +0300

    added actor and movie view models

---
+++ TopMovies.Web.ViewModels/Actors/ActorDetailsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopMovies.Web.ViewModels.MovieCharacters;
+using TopMovies.Web.ViewModels.Movies;
+
+namespace TopMovies.Web.ViewModels.Actors
+{
+	public class ActorDetailsViewModel
+	{
+        public ActorDetailsViewModel()
+        {
+			MoviesKnownFor = new HashSet<MovieViewModel>();
+			MovieCharactersKnownFor = new HashSet<MovieCharacterViewModel>();
+		}
+
+        public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public string Description { get; set; } = null!;
+
+		public string ImageUrl { get; set; } = null!;
+
+		public string DateOfBirth { get; set; } = null!;
+
+		public IEnumerable<MovieViewModel> MoviesKnownFor { get; set; } = null!;
+
+		public IEnumerable<MovieCharacterViewModel> MovieCharactersKnownFor { get; set; } = null!;
+	}
+}
---
+++ TopMovies.Web.ViewModels/Movies/MovieViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopMovies.Web.ViewModels.Movies
+{
+	public class MovieViewModel
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public string ImageUrl { get; set; } = null!;
+
+		public string YearOfRelease { get; set; } = null!;
+	}
+}
