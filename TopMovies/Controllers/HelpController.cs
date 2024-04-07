999ec0f220700ac19a663d41ee7f88f24efd3736
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-04-07 15:48:48 +0300

    added genreViewModel and began making a q&a section

---
+++ TopMovies/Controllers/HelpController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TopMovies.Web.Controllers
+{
+	public class HelpController : BaseController
+	{
+		[AllowAnonymous]
+		public IActionResult QandA()
+		{
+			return View();
+		}
+	}
+}
