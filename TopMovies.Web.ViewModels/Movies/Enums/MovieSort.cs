6249396ea528d5a5ae5db361b588d837d8a1cf8b
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-04-10 17:57:42 +0300

    added movie sort enum

---
+++ TopMovies.Web.ViewModels/Movies/Enums/MovieSort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopMovies.Web.ViewModels.Movies.Enums
+{
+	public enum MovieSort
+	{
+		Newest = 0,
+		Oldest = 1,
+		NameAscending = 2,
+		NameDescending = 3,
+		RatingAscending = 4,
+		RatingDescending = 5,
+	}
+}
