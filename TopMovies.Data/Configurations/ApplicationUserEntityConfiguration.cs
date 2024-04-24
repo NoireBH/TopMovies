fc619c5295e030a9da4e52b615c33665a3eea103
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-04-24 15:37:41 +0300

    add user seed

--- TopMovies.Data/Configurations/ApplicationUserEntityConfiguration.cs
+++ TopMovies.Data/Configurations/ApplicationUserEntityConfiguration.cs
@@ -8,88 +8,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TopMovies.Data.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace TopMovies.Data.Configurations
 {
-	//public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
-	//{
-	//	private readonly UserManager<ApplicationUser> userManager;
+	public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+	{
+		private readonly UserManager<ApplicationUser> userManager;
 
-	//	public ApplicationUserEntityConfiguration(UserManager<ApplicationUser> userManager)
-	//	{
-	//		this.userManager = userManager;
-	//	}
+		public ApplicationUserEntityConfiguration(UserManager<ApplicationUser> userManager)
+		{
+			this.userManager = userManager;
+		}
 
-	//	public void Configure(EntityTypeBuilder<ApplicationUser> builder)
-	//	{
-	//		builder.HasData(SeedUsers());
-	//	}
+		public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+		{
+			builder.HasData(SeedUsers());
+		}
 
-	//	private async Task<ApplicationUser[]> SeedUsers()
-	//	{
-	//		var userManager = serviceProvider
-	//			.GetRequiredService<UserManager<ApplicationUser>>();
+		private async Task<ApplicationUser[]> SeedUsers()
+		{
 
-	//		var users = new HashSet<ApplicationUser>();
+			var users = new HashSet<ApplicationUser>();
 
-	//		string password = "123456";
+			string password = "123456";
 
-	//		ApplicationUser user;
-	//		PasswordHasher<ApplicationUser> passwordHasher;
-	//		string hashed;
+			ApplicationUser user;
+			PasswordHasher<ApplicationUser> passwordHasher;
+			string hashed;
 
-	//		user = new ApplicationUser()
-	//		{
-	//			Id = Guid.Parse("E11F771C-1B10-41EB-81BC-0683DD00B080"),
-	//			UserName = "firstUser",
-	//			NormalizedUserName = "FIRSTUSER",
-	//			Email = "FirstUser@abv.bg",
-	//			NormalizedEmail = "FIRSTUSER@ABV.BG",
-	//			EmailConfirmed = true,
-	//		};
+			user = new ApplicationUser()
+			{
+				Id = Guid.Parse("E11F771C-1B10-41EB-81BC-0683DD00B080"),
+				UserName = "firstUser",
+				NormalizedUserName = "FIRSTUSER",
+				Email = "FirstUser@abv.bg",
+				NormalizedEmail = "FIRSTUSER@ABV.BG",
+				EmailConfirmed = true,
+			};
 
-	//		passwordHasher = new PasswordHasher<ApplicationUser>();
-	//		hashed = passwordHasher.HashPassword(user, password);
-	//		user.PasswordHash = hashed;
+			passwordHasher = new PasswordHasher<ApplicationUser>();
+			hashed = passwordHasher.HashPassword(user, password);
+			user.PasswordHash = hashed;
 
-	//		var result = await userManager.CreateAsync(user, password);
-	//		users.Add(user);
+			var result = await userManager.CreateAsync(user, password);
+			users.Add(user);
 
-	//		user = new ApplicationUser()
-	//		{
-	//			Id = Guid.Parse("2110C067-48D9-441A-A1EB-C723466B60C0"),
-	//			UserName = "secondtUser",
-	//			NormalizedUserName = "SECONDUSER",
-	//			Email = "SecondUser@abv.bg",
-	//			NormalizedEmail = "SECONDUSER@ABV.BG",
-	//			EmailConfirmed = true,
-	//		};
+			user = new ApplicationUser()
+			{
+				Id = Guid.Parse("2110C067-48D9-441A-A1EB-C723466B60C0"),
+				UserName = "secondtUser",
+				NormalizedUserName = "SECONDUSER",
+				Email = "SecondUser@abv.bg",
+				NormalizedEmail = "SECONDUSER@ABV.BG",
+				EmailConfirmed = true,
+			};
 
-	//		passwordHasher = new PasswordHasher<ApplicationUser>();
-	//		hashed = passwordHasher.HashPassword(user, password);
-	//		user.PasswordHash = hashed;
+			passwordHasher = new PasswordHasher<ApplicationUser>();
+			hashed = passwordHasher.HashPassword(user, password);
+			user.PasswordHash = hashed;
 
-	//		var result2 = await userManager.CreateAsync(user, password);
-	//		users.Add(user);
+			var result2 = await userManager.CreateAsync(user, password);
+			users.Add(user);
 
-	//		user = new ApplicationUser()
-	//		{
-	//			Id = Guid.Parse("129D8177-95E3-404F-872E-EB94FE803BD1"),
-	//			UserName = "thirdUser",
-	//			NormalizedUserName = "THIRDUSER",
-	//			Email = "ThirdUser@abv.bg",
-	//			NormalizedEmail = "THIRDUSER@ABV.BG",
-	//			EmailConfirmed = true,
-	//		};
+			user = new ApplicationUser()
+			{
+				Id = Guid.Parse("129D8177-95E3-404F-872E-EB94FE803BD1"),
+				UserName = "thirdUser",
+				NormalizedUserName = "THIRDUSER",
+				Email = "ThirdUser@abv.bg",
+				NormalizedEmail = "THIRDUSER@ABV.BG",
+				EmailConfirmed = true,
+			};
 
-	//		passwordHasher = new PasswordHasher<ApplicationUser>();
-	//		hashed = passwordHasher.HashPassword(user, password);
-	//		user.PasswordHash = hashed;
+			passwordHasher = new PasswordHasher<ApplicationUser>();
+			hashed = passwordHasher.HashPassword(user, password);
+			user.PasswordHash = hashed;
 
-	//		var result3 = await userManager.CreateAsync(user, password);
-	//		users.Add(user);
+			var result3 = await userManager.CreateAsync(user, password);
+			users.Add(user);
 
-	//		return users.ToArray();
-	//	}
-	//}
+			return users.ToArray();
+		}
+	}
 }
